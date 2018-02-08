74ee2e5e8a922cf8e502d1066e154e27d65339da
Mike Killion <killion.kgs@gmail.com>
2018-02-08 10:25:59 -0600

    Intermediate commit.
    Displays map, parses url parameters.

--- main.js
+++ main.js
@@ -1,63 +1,49 @@
-require(["esri/map", "esri/dijit/Scalebar", "dojo/domReady!"], function(Map,Scalebar) {
-    var map = new Map("map-div", {
-        center: [-98.5, 38.25],
-        zoom: 7,
-        basemap: "national-geographic",
-        logo: false
-    });
 
-    var scalebar = new Scalebar({
+require( [
+    "esri/Map",
+    "esri/widgets/ScaleBar",
+    "esri/layers/TileLayer",
+    "esri/views/MapView",
+    "esri/geometry/support/webMercatorUtils",
+    "dojo/domReady!"
+],
+function(
+    Map,
+    ScaleBar,
+    TileLayer,
+    MapView,
+    webMercatorUtils
+) {
+    var plssLayer = new TileLayer( {url:"http://services.kgs.ku.edu/arcgis8/rest/services/plss/plss/MapServer", id:"Section-Township-Range"} );
+	var topoLayer = new TileLayer( {url:"https://basemap.nationalmap.gov/arcgis/rest/services/USGSTopo/MapServer", id:"Topo"} );
+
+    var map = new Map( {
+        layers: [topoLayer, plssLayer]
+    } );
+
+    var view = new MapView( {
         map: map,
-        scalebarUnit: "dual"
-    });
+        container: "map-div",
+        center: [-98, 38],
+        zoom: 7,
+        ui: { components: ["zoom"] },
+		constraints: { rotationEnabled: false }
+    } );
 
-    earthquakesLayer = new esri.layers.ArcGISDynamicMapServiceLayer("http://services.kgs.ku.edu/arcgis1/rest/services/co2/seismic_1/MapServer");
-    earthquakesLayer.setVisibleLayers([8]);
-    map.addLayers([earthquakesLayer]);
-});
+    view.when(function() {
+        urlParams = location.search.substr(1);
+        urlZoom(urlParams);
+    } );
 
-function filterQuakes(year, mag) {
-    var nextYear = parseInt(year) + 1;
-    var def = [];
-
-    if (year !== "all") {
-        if (mag !== "all") {
-            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and net in ('us', ' ', 'US') and mag >=" + mag;
-        } else {
-            def[8] = "the_date >= to_date('" + year + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and the_date < to_date('" + nextYear + "-01-01 00:00:00','YYYY-MM-DD HH24:MI:SS') and mag >= 2 and net in ('us', ' ', 'US')";
+    function urlZoom(params) {
+        var items = urlParams.split("&");
+        if (items.length > 1) {
+            var lat = items[0].substring(4);
+            var lon = items[1].substring(4);
+            var datum = items[2].substring(4);
+            var trs = items[3].substring(4);
         }
-    } else {
-        if (mag !== "all") {
-            def[8] = " mag >=" + mag;
-        } else {
-            def[8] = "";
-        }
+        
+        //
     }
-    earthquakesLayer.setLayerDefinitions(def);
-}
-
-function filterQuakesRecent() {
-    var def = [];
-    def[8] = "state = 'KS' and mag >= 2 and net in ('us', ' ', 'US') and the_date = (select max(the_date) from earthquakes where state = 'KS' and mag >= 2 and net in ('us', ' ', 'US'))";
-    earthquakesLayer.setLayerDefinitions(def);
-}
-
-function filterQuakesDays(days) {
-    var def = [];
-
-    if (days !== "all") {
-        def[8] = "sysdate - the_date <= " + days + " and mag >= 2 and net in ('us', ' ', 'US')";
-    } else {
-        def[8] = "";
-    }
-    earthquakesLayer.setLayerDefinitions(def);
-}
-
-function clearQuakeFilter() {
-    var def = [];
-    def = "";
-    earthquakesLayer.setLayerDefinitions(def);
-    days.options[0].selected="selected";
-    mag.options[0].selected="selected";
-    year.options[0].selected="selected";
-}
+} );
